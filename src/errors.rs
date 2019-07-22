93c1716f6b9e8fefbcd935c2ee6537023300a93e
Alex Pyrgiotis <apyrgio@gmail.com>
2019-07-22 07:44:12 +0000

    Add an explanation for each error type

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,7 @@
 //! # Tindercrypt errors
 
+use std::fmt;
+
 /// The errors that can be returned by the library.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Error {
@@ -18,4 +20,36 @@ pub enum Error {
     MetadataMissing,
     /// The metadata header of the encrypted buffer contains invalid values.
     MetadataInvalid,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::BufferTooSmall => {
+                write!(f, "The provided buffer is shorter than expected")
+            }
+            Error::PassphraseTooSmall => {
+                write!(f, "The provided passphrase is shorter than expected")
+            }
+            Error::KeySizeMismatch => write!(
+                f,
+                "The provided key does not have the required length for the \
+                 encryption algorithm"
+            ),
+            Error::CryptoParamsWeak => write!(
+                f,
+                "The provided parameters for the encryption are too weak"
+            ),
+            Error::DecryptionError => {
+                write!(f, "Could not decrypt the ciphertext")
+            }
+            Error::MetadataMissing => write!(
+                f,
+                "The provided buffer does not include a metadata header"
+            ),
+            Error::MetadataInvalid => {
+                write!(f, "The provided buffer has an invalid metadata header")
+            }
+        }
+    }
 }
