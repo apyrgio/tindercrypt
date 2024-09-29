6735193c1aeea1ba82f58cc736d6571d452431ab
Alex Pyrgiotis <apyrgio@gmail.com>
2024-09-29 16:08:20 +0300

    Bump Protobuf dependencies
    Bump Protobuf creates to the next version. This means that we need to
    update the `build.rs` script that generates Rust code from our Protobuf
    definitions.

--- build.rs
+++ build.rs
@@ -4,19 +4,19 @@
 // https://github.com/stepancheg/rust-protobuf/tree/master/protoc-rust
 
 #[cfg(feature = "proto-gen")]
-use protoc_rust;
+use protobuf_codegen;
 
 // NOTE: When this feature is enabled, the build will always run, even if the
 // .proto file has not changed. This happens because the .rs file always gets
 // updated, since this function unconditionally creates it.
 #[cfg(feature = "proto-gen")]
 fn proto_gen() {
-    protoc_rust::Codegen::new()
+    protobuf_codegen::Codegen::new()
+        .pure()
         .out_dir("proto/")
-        .inputs(&["proto/metadata.proto"])
-        .include(".")
-        .run()
-        .expect("Error during generation of Rust code from .proto files");
+        .includes(&["proto/"])
+        .input("proto/metadata.proto")
+        .run_from_script()
 }
 
 fn main() {
