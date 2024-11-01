bd98f998629fa1aea20d9214ccd56f15d10cc5f4
Alex Pyrgiotis <apyrgio@gmail.com>
2024-11-01 14:44:11 +0200

    Allow missing docs for generate code
    Allow missing docs for Rust code generated from Protobuf definitions.
    The `deny(missing_docs)` rule applies to the whole codebase, but this
    code is ultimately not under our control, so we have to make an
    exception.

--- build.rs
+++ build.rs
@@ -5,6 +5,8 @@
 
 #[cfg(feature = "proto-gen")]
 use protobuf_codegen;
+#[cfg(feature = "proto-gen")]
+use std::fs;
 
 // NOTE: When this feature is enabled, the build will always run, even if the
 // .proto file has not changed. This happens because the .rs file always gets
@@ -16,7 +18,16 @@ fn proto_gen() {
         .out_dir("proto/")
         .includes(&["proto/"])
         .input("proto/metadata.proto")
-        .run_from_script()
+        .run_from_script();
+
+    // Allow missing docs for the generated code, since its not under our
+    // control.
+    let proto_mod_rs =
+        fs::read_to_string("proto/mod.rs").expect("Unable to open file");
+    let header = "// Header added by Tindercrypt's build.rs script\n\
+                  #![allow(missing_docs)]\n\n";
+    let new_proto_mod_rs = header.to_owned() + &proto_mod_rs;
+    fs::write("proto/mod.rs", new_proto_mod_rs).expect("Unable to write file");
 }
 
 fn main() {
--- proto/mod.rs
+++ proto/mod.rs
@@ -1,5 +1,6 @@
-// @generated
-
+// Header added by Tindercrypt's build.rs script
 #![allow(missing_docs)]
+
+// @generated
 
 pub mod metadata;
